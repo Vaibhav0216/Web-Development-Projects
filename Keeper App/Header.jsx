eadd44abd3d9f07ca02f73f76a0de5d3baf057e2
Vaibhav Kshirsagar <92253900+Vaibhav-Kshirsagar@users.noreply.github.com>
2023-06-07 06:37:35 +0530

    Create Header.jsx

---
+++ Keeper App/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header=()=>{
+    return(<>
+        <header>
+         <img src={require("./logo.jpg")} alt="" width="50px" height="50px"/>
+         <h1>Vaibhav Keep</h1>
+        </header>
+    </>);
+} 
+
+export default Header;
